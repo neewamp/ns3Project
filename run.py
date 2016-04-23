f0859d6fd27a3c7c263deab7c4fb9f223b6e8322
neewamp <neewamp@gmail.com>
2016-04-23 15:39:45 -0400

    Python script to run program

---
+++ run.py
@@ -0,0 +1,16 @@
+import os
+out = open('Tdma.txt','w')
+
+for i in range(1,101):
+    
+    inp = open('in.txt','w')
+    #print(inp.readlines())
+    print(str(i), file=inp,end='')
+    throughput = os.popen("./waf --run firstTdma <in.txt 2>&1 > /dev/null | grep 'received' > out.txt ;./through.py")
+    through = 0
+    for line in throughput:
+        through = line
+        #print(through)
+    print(str(i), through, file=out, end='')
+
+#os.system("cut output.txt -c3- | cat > output.txt")
