864d7ac4c7f6f5bf8f17a8b16fea36e6ccf6ef04
neewamp <neewamp@gmail.com>
2016-04-25 00:21:23 -0400

    Scripts to find throughput

--- run.py
+++ run.py
@@ -1,16 +1,17 @@
+#!/usr/bin/python3 
 import os
-out = open('ns3Project/graphData/newGraphs/','w')
+out = open('ns3Project/graphData/newGraphs/delayPacketSize/Csma','w')
 
-for i in range(1,30):
-    
+for i in range(1,50):
+   # i /= 10
+    i *= 1024#Use me for packet size
     inp = open('in.txt','w')
-    #print(inp.readlines())
-    print(str(i), file=inp,end='')
-    throughput = os.popen("./waf --run firstTdma <in.txt 2>&1 > /dev/null | grep 'received' > out.txt ;./through.py")
+    print(str(i), file=inp ,end='')
+    throughput = os.popen("./waf --run csmaTest <in.txt 2>&1 > /dev/null | grep 'received' > out.txt ;./through.py")
     through = 0
     for line in throughput:
         through = line
-        #print(through)
-    print(str(i), through, file=out, end='')
+    if through != 0:
+        print(str(i), through, file=out, end='')
 
 #os.system("cut output.txt -c3- | cat > output.txt")
--- through.py
+++ through.py
@@ -1,21 +1,27 @@
+#!/usr/bin/python3
+
 import os
 
 
-from sys import stdin
-
-
+#from sys import stdin
 
 lnum = 0
 words = os.popen("wc out.txt")
-headntail = os.popen("(head -n1 && tail -n1) < out")
+headntail = os.popen("(head -n1 && tail -n1) < out.txt")
 for word in words:
-    lnum = int(word[2:4])
+  #lnum = int(word.split(" ")[1])
+  list = word.split(" ")
+  newList = []
+  for elem in list:
+    if elem != '':
+      newList.append(elem)
+  lnum = int(newList[0])
 
 delay = []
 for line in headntail:
-    delay.append(float(line[:-1].split(" ")[2][:-1]))
+  delay.append(float(line[:-1].split(" ")[2][:-1]))
 delay = delay[1] - delay[0]
 throughput = (1024 * lnum) / delay
-
+#throughput = (lnum) / delay
 print(throughput)
-    
+#print(delay)    
